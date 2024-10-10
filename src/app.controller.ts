af002fe2887465eaed0d5e64d1179f96d1578499
ZineddineBk09 <eino1209@gmail.com>
2024-10-10 10:04:57 +0100

    add auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,51 +1,14 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Post,
-  Body,
-  Put,
-  Param,
-  Delete,
-  Res,
-  HttpStatus,
-} from '@nestjs/common';
-import {
-  CreateCatDto,
-  UpdateCatDto,
-  ListAllEntities,
-} from './dto/create-cat.dto';
-import { Response } from 'express';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
-@Controller('cats')
+@Controller()
 export class AppController {
-  @Post()
-  create(@Res() res: Response, @Body() createCatDto: CreateCatDto) {
-    res.status(HttpStatus.CREATED).send();
-  }
+  constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
-  findAll(
-    @Res({ passthrough: true }) res: Response,
-    @Query() query: ListAllEntities,
-  ) {
-    // return `This action returns all cats (limit: ${query.limit} items)`;
-    res.status(HttpStatus.OK).json([]);
-    return []
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
