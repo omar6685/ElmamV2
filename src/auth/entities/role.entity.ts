530dd722f9c6694d043e3ab91fcd5c36e360683c
ZineddineBk09 <eino1209@gmail.com>
2024-10-13 21:31:38 +0100

    auth entities

---
+++ src/auth/entities/role.entity.ts
@@ -0,0 +1,36 @@
+import { User } from 'src/users/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Timestamp,
+  PrimaryColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Timestamp;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+  })
+  updated_at: Timestamp;
+}
+
+@Entity('users_roles')
+export class UserRole {
+  @PrimaryColumn()
+  user_id: number;
+
+  @PrimaryColumn()
+  role_id: number;
+}
