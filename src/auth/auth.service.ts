d33f9dd172fb599123481fb8aa444eb963c40633
ZineddineBk09 <eino1209@gmail.com>
2024-10-13 22:06:10 +0100

    add roles to signup and login users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { Role } from './entities/role.entity';
 
 @Injectable()
 export class AuthService {
@@ -10,25 +11,30 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
+  // Sign in method
   async signIn(
     email: string,
     password: string,
   ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(email);
     if (!user) {
-      throw new UnauthorizedException('Invalid credentials'); // More descriptive error message
+      throw new UnauthorizedException('Invalid credentials');
     }
 
     // Compare hashed password with provided password
     const isPasswordValid = await bcrypt.compare(
       password,
       user.encrypted_password,
-    ); // Assuming encryptedPassword is hashed
+    );
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const payload = { sub: user.id, email: user.email,roles:user.roles };
+    // Fetch user roles
+    const roles: Role[] = await this.usersService.getUserRoles(user.id);
+    const roleNames = roles.map((role) => role.name); // Extract role names
+
+    const payload = { sub: user.id, email: user.email, roles: roleNames };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
@@ -53,7 +59,7 @@ export class AuthService {
       encrypted_password: hashedPassword,
     });
 
-    const payload = { sub: newUser.id, email: newUser.email };
+    const payload = { sub: newUser.id, email: newUser.email, roles: [] };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
