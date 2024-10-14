b485c7bf056745c37c4db0f70287b26186f3c66e
ZineddineBk09 <eino1209@gmail.com>
2024-10-14 10:27:46 +0100

    migrations

---
+++ src/migrations/1728833444790-init.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1728833444790 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
