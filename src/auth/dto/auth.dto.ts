f14664f6d11b26b512fe4bea314804a6f5b1947a
ZineddineBk09 <eino1209@gmail.com>
2024-10-14 10:28:24 +0100

    use zod for dtos

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,25 @@
-export class SignInDto{
-    email: string;
-    password: string;
-}
+import { z } from 'zod';
+import validator from "validator";
 
-export class SignupDto{
-    email: string;
-    password: string;
-}
+// Define Zod schema for SignInDto
+export const signInSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+  .required();
+
+export type SignInDto = z.infer<typeof signInSchema>;
+
+// Define Zod schema for SignUpDto
+export const signUpSchema = z
+  .object({
+    first_name: z.string(),
+    last_name: z.string(),
+    phone: z.string().min(10).refine(validator.isMobilePhone),
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+  .required();
+
+export type SignUpDto = z.infer<typeof signUpSchema>;
