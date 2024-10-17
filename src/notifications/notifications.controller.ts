9beef29036c2c7914ee1299ec96d23c3cceb811c
ZineddineBk09 <eino1209@gmail.com>
2024-10-17 08:45:46 +0100

    update notification module

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   UseGuards,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
@@ -21,24 +23,28 @@ export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER, RolesEnum.TECHNICAL)
   create(@Body() createNotificationDto: CreateNotificationDto) {
     return this.notificationsService.create(createNotificationDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER, RolesEnum.TECHNICAL)
   findAll() {
     return this.notificationsService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER, RolesEnum.TECHNICAL)
   findOne(@Param('id') id: string) {
     return this.notificationsService.findOne(+id);
   }
 
   @Patch(':id')
+  @HttpCode(HttpStatus.OK)
   @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER, RolesEnum.TECHNICAL)
   update(
     @Param('id') id: string,
@@ -48,8 +54,20 @@ export class NotificationsController {
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER, RolesEnum.TECHNICAL)
   remove(@Param('id') id: string) {
     return this.notificationsService.remove(+id);
+  }
+
+  @Post('send')
+  @HttpCode(HttpStatus.OK)
+  @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER, RolesEnum.TECHNICAL)
+  async sendNotification(
+    @Body('userId') userId: number,
+    @Body('title') title: string,
+    @Body('body') body: string,
+  ) {
+    return this.notificationsService.send(userId, title, body);
   }
 }
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { NotificationsService } from './notifications.service';
 import { User } from 'src/users/entities/user.entity';
+import { NotificationsController } from './notifications.controller';
 import { Message } from '../messages/entities/message.entity';
 import { Notification } from './entities/notification.entity';
+import { NotificationToken } from './entities/notification-token.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Message, Notification])],
+  imports: [
+    TypeOrmModule.forFeature([User, Message, Notification, NotificationToken]),
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
