75d26fb5bf3026a68f6a9f767acf8f1459053f80
ZineddineBk09 <eino1209@gmail.com>
2024-10-23 17:21:28 +0100

    emails scheduled endpoints

--- src/emails/emails.controller.ts
+++ src/emails/emails.controller.ts
@@ -7,7 +7,10 @@ import {
   UseGuards,
   HttpCode,
   HttpStatus,
+  Delete,
+  UsePipes,
 } from '@nestjs/common';
+
 import { EmailsService } from './emails.service';
 import { RolesGuard } from 'src/shared/guards/roles.guard';
 import { RolesEnum } from 'src/shared/enums/role.enum';
@@ -15,6 +18,16 @@ import { Roles } from 'src/shared/decorators/roles.decorator';
 import { SendEmailDto } from './dto/send-email.dto';
 import { SendEmailFromTemplateDto } from './dto/send-email-from-template.dto';
 import { GetTemplateByNameDto } from './dto/get-template.dto';
+import { ZodValidationPipe } from 'src/shared/pipes/zod.pipe';
+import {
+  cancelScheduledEmailDto,
+  CancelScheduledEmailDto,
+} from './dto/cancel-schedule-email.dto';
+import { scheduleEmailDto, ScheduleEmailDto } from './dto/schedule-email.dto';
+import {
+  scheduleRecurringEmailDto,
+  ScheduleRecurringEmailDto,
+} from './dto/schedule-recurring.dto';
 
 @Controller('emails')
 @UseGuards(RolesGuard)
@@ -61,5 +74,53 @@ export class EmailsController {
   @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER)
   async getTemplateByName(@Param() getTemplateByNameDto: GetTemplateByNameDto) {
     return this.emailsService.getTemplateByName(getTemplateByNameDto.name);
+  }
+
+  // Get al scheduled emails
+  @Get('scheduled-batches')
+  @HttpCode(HttpStatus.OK)
+  @Roles(RolesEnum.ADMIN)
+  async getAllScheduledBatches() {
+    return this.emailsService.getAllScheduledBatches();
+  }
+
+  // Schedule a one-time email
+  @Post('schedule')
+  @HttpCode(HttpStatus.ACCEPTED)
+  @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER)
+  @UsePipes(new ZodValidationPipe(scheduleEmailDto))
+  async scheduleEmail(@Body() scheduleEmailDto: ScheduleEmailDto) {
+    const { to, subject, content, sendDate } = scheduleEmailDto;
+    return this.emailsService.scheduleEmail(
+      to,
+      subject,
+      content,
+      new Date(sendDate),
+    );
+  }
+
+  // Schedule a recurring email
+  @Post('schedule-recurring')
+  @HttpCode(HttpStatus.ACCEPTED)
+  @Roles(RolesEnum.ADMIN, RolesEnum.CUSTOMER)
+  @UsePipes(new ZodValidationPipe(scheduleRecurringEmailDto))
+  async scheduleRecurringEmail(
+    @Body() scheduleRecurringEmailDto: ScheduleRecurringEmailDto,
+  ) {
+    const { to, subject, content, interval } = scheduleRecurringEmailDto;
+    return this.emailsService.scheduleRecurringEmail(
+      to,
+      subject,
+      content,
+      interval,
+    );
+  }
+
+  // Cancel scheduled email
+  @Delete('cancel/:jobName')
+  @Roles(RolesEnum.ADMIN)
+  @UsePipes(new ZodValidationPipe(cancelScheduledEmailDto))
+  async cancelScheduledEmail(@Param('jobName') jobName: string) {
+    return this.emailsService.cancelScheduledEmail(jobName);
   }
 }
