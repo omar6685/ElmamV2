d5bf6da74caf67f3461498117af5a811382e00c5
ZineddineBk09 <eino1209@gmail.com>
2024-10-28 17:50:07 +0100

    wathq api response

---
+++ src/crns/types/wathq-response.type.ts
@@ -0,0 +1,49 @@
+export type TWathqInfoReponse = {
+  crName: string;
+  crNumber: string;
+  crEntityNumber: string;
+  issueDate: string;
+  expiryDate: string;
+  crMainNumber: string;
+  crMainEntityNumber: string;
+  businessType: {
+    id: string;
+    name: string;
+  };
+  fiscalYear: {
+    month: number;
+    day: number;
+    calendarType: {
+      id: number;
+      name: string;
+    };
+  };
+  status: {
+    id: string;
+    name: string;
+    nameEn: string;
+  };
+  cancellation: {
+    date: string;
+    reason: string;
+  };
+  location: {
+    id: string;
+    name: string;
+  };
+  company: {
+    period: string;
+    startDate: string;
+    endDate: string;
+  };
+  activities: {
+    description: string;
+    isic: [
+      {
+        id: string;
+        name: string;
+        nameEn: string | null;
+      },
+    ];
+  };
+};
