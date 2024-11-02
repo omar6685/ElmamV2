9b0e888e760354d3df55f136fddfdc0438b56e13
ZineddineBk09 <eino1209@gmail.com>
2024-11-02 20:52:57 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
 
   // Setup Swagger
   const config = new DocumentBuilder()
