d60289711c1cd53108a05a8c776665db4e6a90b8
ZineddineBk09 <eino1209@gmail.com>
2024-11-02 08:35:12 +0100

    crn entity to handle organization files upload

---
+++ src/entities/entities/crn-entity.entity.ts
@@ -0,0 +1,73 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { CommercialRegistrationNumber } from 'src/crns/entities/crn.entity';
+import { Entities } from './entity.entity';
+
+@Index(
+  'index_crn_entities_on_commercial_registration_number_id',
+  ['commercialRegistrationNumberId'],
+  {},
+)
+@Index('index_crn_entities_on_entity_id', ['entityId'], {})
+@Index('crn_entities_pkey', ['id'], { unique: true })
+@Entity('crn_entities', { schema: 'public' })
+export class CrnEntities {
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id' })
+  id: string;
+
+  @Column('bigint', { name: 'entity_id' })
+  entityId: string;
+
+  @Column('character varying', { name: 'logo_url' })
+  logoUrl: string;
+
+  @Column('boolean', { name: 'adaptation', nullable: true })
+  adaptation: boolean | null;
+
+  @Column('timestamp without time zone', { name: 'created_at' })
+  createdAt: Date;
+
+  @Column('timestamp without time zone', { name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column('bigint', { name: 'commercial_registration_number_id' })
+  commercialRegistrationNumberId: string;
+
+  @Column('character varying', { name: 'xlsx_file_local', nullable: true })
+  xlsxFileLocal: string | null;
+
+  @Column('character varying', {
+    name: 'subscribers_xlsx_file',
+    nullable: true,
+  })
+  subscribersXlsxFile: string | null;
+
+  @Column('character varying', {
+    name: 'resident_xlsx_file',
+    nullable: true,
+  })
+  residentXlsxFile: string | null;
+
+  @ManyToOne(
+    () => CommercialRegistrationNumber,
+    (commercialRegistrationNumbers) =>
+      commercialRegistrationNumbers.crnEntities,
+  )
+  @JoinColumn([
+    { name: 'commercial_registration_number_id', referencedColumnName: 'id' },
+  ])
+  commercialRegistrationNumber: CommercialRegistrationNumber;
+
+  @ManyToOne(() => Entities, (entities) => entities.crnEntities)
+  @JoinColumn([{ name: 'entity_id', referencedColumnName: 'id' }])
+  entity: Entities;
+}
+
+// add logourl (on cloudinary), adaptation (taha9o9), new excel files for: subscribers, resident (mo9im),
