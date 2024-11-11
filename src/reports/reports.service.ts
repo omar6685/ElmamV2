53979b43da1f00ab95e5eb4040970238adce66bd
Zineddine Benkhaled <86568908+ZineddineBk09@users.noreply.github.com>
2024-11-11 17:40:19 +0100

    change nationality report generation logic to generate report for all companies inside the same entity

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -5,17 +5,60 @@ import { Repository } from 'typeorm';
 import { CreateNationalityReportDto } from './dto/nationality-reports/create.dto';
 import { UpdateNationalityReportDto } from './dto/nationality-reports/update.dto';
 import { NationalityReport } from './entities/nationality-report.entity';
+import { CrnEntities } from 'src/entities/entities/crn-entity.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(
     @InjectRepository(NationalityReport)
     private readonly nationalityReportsRepository: Repository<NationalityReport>,
+    @InjectRepository(CrnEntities)
+    private readonly crnEntitiesRepository: Repository<CrnEntities>,
   ) {}
   async create(createReportDto: CreateNationalityReportDto) {
     try {
+      // const { saudis, totalEmployees, result, maxAddition } =
+      //   this.extractReportFields(createReportDto);
+      // const newReport: NationalityReport =
+      //   this.nationalityReportsRepository.create({
+      //     name: `Report-${new Date().getTime().toString()}`,
+      //     saudis,
+      //     totalEmployees,
+      //     result,
+      //     maxAddition,
+      //     entityId: createReportDto.entityId,
+      //     userId: createReportDto.userId,
+      //   });
+      // await this.nationalityReportsRepository.save(newReport);
+      // return newReport;
+      // change the logic of the create method to the following
+      // when receiving a request to generate a nationality report the body will only contain the entityId
+      // we will use crnEntitiesRepository to get all the CRNs that belong to the entity
+      // every crn will have a nationalities field which an aray of objects containings nationalities and their count
+      // for ex: "[{""name"": ""هندي"", ""count"": 31}, {""name"": ""فلبيني"", ""count"": 1}, {""name"": ""نيبالي"", ""count"": 7}, {""name"": ""باكستاني"", ""count"": 4}, {""name"": ""مصرى"", ""count"": 5}, {""name"": ""سعودي"", ""count"": 8}, {""name"": ""يمني"", ""count"": 15}, {""name"": ""سوداني"", ""count"": 1}]"
+      // after that we will calculate the total number of employees and the number of saudis and the result field for all the nationalities inside the crn entities and combine them in one report
+
+      const crnEntities = await this.crnEntitiesRepository.find({
+        where: { entityId: createReportDto.entityId },
+      });
+
+      console.log('CrnEntities that belong to the entity', crnEntities);
+
+      const nationalities = crnEntities.reduce(
+        (acc, crn) => {
+          const crnNationalities = crn.nationalities || [];
+          return acc.concat(crnNationalities);
+        },
+        [] as { name: string; count: number }[],
+      );
+
+      console.log('Nationalities extracted from the CRNs', nationalities);
+
       const { saudis, totalEmployees, result, maxAddition } =
-        this.extractReportFields(createReportDto);
+        this.extractReportFields({ nationalities });
+
+      console.log('saudis', result);
+
       const newReport: NationalityReport =
         this.nationalityReportsRepository.create({
           name: `Report-${new Date().getTime().toString()}`,
@@ -26,7 +69,9 @@ export class ReportsService {
           entityId: createReportDto.entityId,
           userId: createReportDto.userId,
         });
+
       await this.nationalityReportsRepository.save(newReport);
+
       return newReport;
     } catch (err) {
       console.log(err);
@@ -65,7 +110,10 @@ export class ReportsService {
     await this.nationalityReportsRepository.remove(nationalityReport);
   }
 
-  private extractReportFields(data: CreateNationalityReportDto) {
+  private extractReportFields(data: {
+    nationalities: { name: string; count: number }[];
+  }) {
+    console.log('extractReportFields called with data', data);
     // Calculate the total number of employees
     const totalEmployees = data.nationalities.reduce(
       (sum, { count }) => sum + count,
