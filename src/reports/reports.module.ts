c30f0eab5e3902d0787fdd4ace73792561394238
Zineddine Benkhaled <86568908+ZineddineBk09@users.noreply.github.com>
2024-11-11 17:39:41 +0100

    add CrnEntities

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { NationalityReport } from './entities/nationality-report.entity';
+import { CrnEntities } from 'src/entities/entities/crn-entity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NationalityReport])],
+  imports: [TypeOrmModule.forFeature([NationalityReport, CrnEntities])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
