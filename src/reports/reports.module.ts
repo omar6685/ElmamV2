91c933d6c9b3eadd5b98ea663d70b47ecdb6898b
Zineddine Benkhaled <86568908+ZineddineBk09@users.noreply.github.com>
2024-11-12 21:11:01 +0100

    add CommercialRegistrationNumber

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -5,9 +5,16 @@ import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { NationalityReport } from './entities/nationality-report.entity';
 import { CrnEntities } from 'src/entities/entities/crn-entity.entity';
+import { CommercialRegistrationNumber } from 'src/crns/entities/crn.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NationalityReport, CrnEntities])],
+  imports: [
+    TypeOrmModule.forFeature([
+      NationalityReport,
+      CrnEntities,
+      CommercialRegistrationNumber,
+    ]),
+  ],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
